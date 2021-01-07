1c303348ceb79671b3a90342d2037ac855aa96fb
xScoopy <JLeary070@gmail.com>
2021-01-07 17:45:57 -0600

    got functionality working for searching by genre or title

--- app.py
+++ app.py
@@ -17,36 +17,53 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 GENRE_URL = f'https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}&language=en-US'
 MOVIE_URL = f'https://api.themoviedb.org/3/discover/movie'
+SEARCH_URL = f'https://api.themoviedb.org/3/search/movie'
 pp = PrettyPrinter(indent=4)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    """Show a form to search for GIFs and show resulting GIFs from Tenor API."""
+    """Shows a form to search for movies, then shows results beneath the form."""
     if request.method == 'POST':
         genre = request.form.get('pick_genre')
-        response = requests.get(MOVIE_URL,
-        {
-            'api_key' : API_KEY, 
-            'with_genres' : genre
+        title = request.form.get('pick_title')
+        genre_response = requests.get(GENRE_URL)
+        genre_result = json.loads(genre_response.content).get('genres')
+        if not title:
+            response = requests.get(MOVIE_URL,
+                                    {
+                                        'api_key': API_KEY,
+                                        'with_genres': genre
 
-        }
-        )
-        result = json.loads(response.content).get('results')
-        pp.pprint(result)
-        context = {
-            'movies' : result
-        }
-        return render_template('home.html', **context)
+                                    }
+                                    )
+            result = json.loads(response.content).get('results')
+            # pp.pprint(result)
+            context = {
+                'movies': result,
+                'results': genre_result
+            }
+            return render_template('home.html', **context, )
+        else:
+            response = requests.get(SEARCH_URL,
+                                    {
+                                        'api_key': API_KEY,
+                                        'query': title
+                                    })
+            result = json.loads(response.content).get('results')
+            context = {
+                'movies':result, 
+                'results':genre_result
+            }
+            return render_template('home.html', **context)
     else:
         response = requests.get(GENRE_URL)
         result = json.loads(response.content).get('genres')
-        #pp.pprint(result)
+        # pp.pprint(result)
         context = {
-            'results' : result
+            'results': result
         }
         return render_template('home.html', **context)
-
-
-
 
 
 if __name__ == '__main__':
